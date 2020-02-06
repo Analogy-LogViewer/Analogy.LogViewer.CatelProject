30258bd4921c1f14fea9713f9f8f1018b19472cb
Lior Banai <liorbanai@gmail.com>
2020-02-06 21:45:19 +0200

    initial

---
+++ Analogy.LogViewer.CatelProject/DataProvidersFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Analogy.Interfaces;
+using Analogy.Interfaces.Factories;
+
+namespace Analogy.LogViewer.CatelProject
+{
+    public class DataProvidersFactory : IAnalogyDataProvidersFactory
+    {
+        public string Title { get; } = "Some Title";
+        public IEnumerable<IAnalogyDataProvider> Items { get; }
+
+        public DataProvidersFactory()
+        {
+            //get some data provider
+            List<IAnalogyDataProvider> dataProviders = new List<IAnalogyDataProvider>();
+            dataProviders.Add(new OfflineDataProvider());
+        
+        }
+    }
+}
---
+++ Analogy.LogViewer.CatelProject/OfflineDataProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Analogy.Interfaces;
+
+namespace Analogy.LogViewer.CatelProject
+{
+   public class OfflineDataProvider:IAnalogyOfflineDataProvider
+    {
+  
+        public Guid ID { get; }
+        public string OptionalTitle { get; }
+        public bool CanSaveToLogFile { get; }
+        public string FileOpenDialogFilters { get; }
+        public string FileSaveDialogFilters { get; }
+        public IEnumerable<string> SupportFormats { get; }
+        public string InitialFolderFullPath { get; }
+        public Task<IEnumerable<AnalogyLogMessage>> Process(string fileName, CancellationToken token, ILogMessageCreatedHandler messagesHandler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FileInfo> GetSupportedFiles(DirectoryInfo dirInfo, bool recursiveLoad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(List<AnalogyLogMessage> messages, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanOpenFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanOpenAllFiles(IEnumerable<string> fileNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InitializeDataProviderAsync(IAnalogyLogger logger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageOpened(AnalogyLogMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
